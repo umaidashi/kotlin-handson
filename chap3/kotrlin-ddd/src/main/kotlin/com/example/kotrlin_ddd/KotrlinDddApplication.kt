fb659054eb8a13db2255d41ecac91ae68efa1c9a
umaidashi <umaidashi18@gmail.com>
2024-08-12 21:24:24 +0900

    feat: kotlin-ddd initial

---
+++ chap3/kotrlin-ddd/src/main/kotlin/com/example/kotrlin_ddd/KotrlinDddApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotrlin_ddd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotrlinDddApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotrlinDddApplication>(*args)
+}
---
+++ chap3/kotrlin-ddd/src/test/kotlin/com/example/kotrlin_ddd/KotrlinDddApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotrlin_ddd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotrlinDddApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
