6d8c1476a586b126711c0d5d51ef0c13ac86d1c6
umaidashi <umaidashi18@gmail.com>
2024-08-12 21:31:28 +0900

    feat: add dokka (documentation for kotlin)

--- chap3/kotrlin-ddd/build.gradle.kts
+++ chap3/kotrlin-ddd/build.gradle.kts
@@ -4,8 +4,9 @@ plugins {
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
 
-	//	detekt
+	//	added
 	id("io.gitlab.arturbosch.detekt") version "1.23.5"
+	id("org.jetbrains.dokka") version "1.9.10"
 }
 
 group = "com.example"
@@ -21,6 +22,14 @@ repositories {
 	mavenCentral()
 }
 
+configurations.matching { it.name.startsWith("dokka") }.configureEach {
+	resolutionStrategy.eachDependency {
+		if (requested.group.startsWith("com.fasterxml.jackson")) {
+			useVersion("2.15.3")
+		}
+	}
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -30,6 +39,7 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 	detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.21.0")
+	dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.9.20")
 }
 
 kotlin {
